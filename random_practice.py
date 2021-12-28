0bf0d59f037e6845840cb8d7d28c314618c11b82
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-12-28 15:12:27 -0700

    Update random_practice.py

--- random_practice.py
+++ random_practice.py
@@ -145,6 +145,145 @@ print('Letters:', letters)
 print('Sorted Letters:', sorted_letters)
 print('Descending Letters:', desc_letters)
 
+# How do I flip a list? (Reversing)
+
+#reversed_employees = reversed(employees) #this will give you an iteration not list.
+reversed_employees = list(reversed(employees))
+print(employees)
+print(reversed_employees)
+
+employees.reverse() #another way to reverse the list
+print(employees)
+print()
+#slicing lists
+letters = list('abcdefghijklmnop')
+print(letters)
+#How do I get letters 'c' through 'f'?
+selected = letters[2:6]
+print('Selected: ', selected)
+#How do I get every other letter/element from the list?
+every_other = letters[0:len(letters):2] #start, stop, step /or letters[::2]
+print('Every other letter: ', every_other)
+print()
+#Reversing with slicing
+reverse_employees = employees[::-2]
+print('Reversed : ', reverse_employees)
+print('Employees : ', employees)
+print()
+#Mathematical Operations: +, -, /, *, %, **,//
+print('3 + 1 is :',3 + 1)
+print('3 - 1 is :',3 - 1)
+print('27 / 9 is :',27 / 9)
+print('27 // 9 is :',27 // 9)
+print('7 % 2 is : ', 7 % 2)
+print('7 * 2 is : ', 7 * 2)
+print('7 ** 2 is : ', 7 ** 2)
+print()
+#What about other types? (String)
+print('a' + 'b') #concatenation operator
+print('Kerri' * 4)
+print()
+#Comparison Operators: <, <=, >, >=, ==
+print( 3 < 4)
+print( 3 <= 4)
+print( 3 > 4)
+print( 3 >= 4)
+print( 3 == 4)
+print( 10 == 10)
+
+word_one = 'Cheese'
+word_two = 'Salami'
+print('Is Cheese < Salami? :', word_one < word_two)
+word_one = 'Cheese'
+word_two = 'Crackers'
+print('Is Cheese < Crackers? :', word_one < word_two)
+print()
+#Determining operator precedence:
+# is, not, in
+# membership tests
+employees = ['Kerri', 'Joe', 'Daniel', 'Kristy', 'Noel']
+print('Kerri in Employees: ', 'Kerri' in employees)
+print('Justin in Employees: ', 'Justin' in employees)
+print('Justin not in Employees: ', 'Justin' not in employees)
+print()
+# identity tests
+fav_food = 'pizza'
+lunch_today = 'pizza'
+print('Is my favorite food lunch today? ', fav_food is lunch_today)# is (not the same as ==)
+other_employees = ['Kerri', 'Joe', 'Daniel', 'Kristy', 'Noel']
+print('Are these employees the same? ', employees is other_employees)
+#not operator
+print(not 1 == 1)
+print(1 != 1)
+#precedence
+print( 4 * 3 + 1 )
+print()
+# if/else statements:
+eaten_brussel_sprouts = True
+                
+if eaten_brussel_sprouts:
+    print('Yayyyy! You get a cookie!')
+else:
+    print('Sorry, You get nothing!!!')
+           
+#PySoy can be used as a game engine.
+#Game
+guess = 7
+if guess == 7:
+    print('You win!')
+elif guess == 8: # elif can be used as an 'or' this.
+    print('runner up!')
+elif guess == 6:
+    print('runner up!')   
+else:
+    print('uh oh')
+
+#compound conditional expressions: AND/OR
+eaten_peas = True
+eaten_carrots = False
+if eaten_peas and eaten_carrots:
+    print('You get ice cream')
+else:
+    print('Nothing for you!!')
+print()
+#This is the inclusive OR example.
+#eaten_peas = True
+#eaten_carrots = False
+#if eaten_peas or eaten_carrots:
+#    print('You get ice cream')
+#else:
+#    print('Nothing for you!!')
+
+
+give_money = True
+fuel_up = True
+
+if give_money and fuel_up:#exclusive or
+     print('No, that is too generous!')
+elif give_money or fuel_up:
+     print('Thank you!')
+else:
+    print('I need your help!')
+print()
+#For loops:
+employees = ['Bob', 'Sue', 'Justin', 'Harry', 'Kerri', 'Jon']
+attempts = range(3)
+print('Employees: ', employees)
+for employee in employees:
+    print('Employee: ', employee)
+fav_number = 13
+for attempt in attempts:
+    guess = int(input("What is my favorite number? \n"))
+    if guess == fav_number:
+       print('YAY, You win!')
+       break
+    else:
+        print('Try again.')
+        print()
+#print('This is how many attempts it took to guess: ',attempt)    
+
+for index, employee in enumerate(employees):
+    print(index, ':', employee)
 
 
 
