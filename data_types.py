9e62aec6c4a0d853e4eb6075ed75b53d3318f8cb
Kerri Cool <79030261+kcool3@users.noreply.github.com>
2021-12-30 15:54:19 -0700

    Update and rename random_practice.py to data_types.py

---
+++ data_types.py
@@ -0,0 +1,34 @@
+#Basic examples of data types in Python.
+
+#This is a boolean example
+likes_oranges = True
+likes_apples = False
+# The equals sign is used to assign the variable.
+print("This person likes oranges:")
+print(likes_oranges)
+print("This person likes apples:")
+print(likes_apples)
+
+#These are examples of string variables.
+favorite_color = 'Purple' # you can use single or double quotes.
+print("This is my favorite color:")
+print(favorite_color)
+
+#This is an example of an integer variable.
+favorite_number = 13
+print("This is my favorite number:")
+print(favorite_number)
+
+#This is an example of a float variable.
+tax_rate = 6.5
+print("This is the current tax rate:")
+print(tax_rate)
+
+
+
+
+
+
+
+
+
